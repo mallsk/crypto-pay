6ce2c86036186b4ac784ca4bc5d52b58c628ff97
Mallikarjun <skollermallikarjun@gmail.com>
2025-05-10 00:36:59 +0530

    back to init

--- components/ui/WalletConnector.tsx
+++ components/ui/WalletConnector.tsx
@@ -8,43 +8,19 @@ import {
 import {
   WalletModalProvider
 } from "@solana/wallet-adapter-react-ui";
-
+import { 
+  UnsafeBurnerWalletAdapter
+} from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
-import {
-  SolanaMobileWalletAdapter,
-  createDefaultAuthorizationResultCache,
-  createDefaultAddressSelector,
-  createDefaultWalletNotFoundHandler
-} from "@solana-mobile/wallet-adapter-mobile";
-
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
-
-// Required for modal styling
-import "@solana/wallet-adapter-react-ui/styles.css";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 export const WalletConnector: FC<{ children: ReactNode }> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(
-    () => [
-      new SolanaMobileWalletAdapter({
-        appIdentity: {
-          name: "Crypto-Pay",
-          uri: "https://solpay.mkdevs.in",
-          icon: "https://solpay.mkdevs.in/favicon.ico",
-        },
-        authorizationResultCache: createDefaultAuthorizationResultCache(),
-        addressSelector: createDefaultAddressSelector(), // New addition
-        onWalletNotFound: createDefaultWalletNotFoundHandler(), 
-        cluster: "devnet",
-      }),
-      new UnsafeBurnerWalletAdapter(),
-    ],
-    []
-  );
+  const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
